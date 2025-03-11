279242cb7f23443262112c68fd56c4c2825733d2
Henning von Bargen <h.vonbargen@t-p.com>
2025-03-11 17:09:45 +0100

    If passphrase not supplied on command line, read it from stdin

--- hvbtest.cmd
+++ hvbtest.cmd
@@ -2,5 +2,5 @@
 setlocal
 set JAVA_HOME=C:\prog\jdk\17\x64\jdk-17.0.5+8
 set PATH=C:\prog\jdk\17\x64\jdk-17.0.5+8\bin;%PATH%
-java -jar target\openpdfsign-0.0.0-dev.162.dirty+d5dbc88e-jar-with-dependencies.jar %*
+java -jar target\openpdfsign-0.0.0-dev.163.dirty+22cf8a5d-jar-with-dependencies.jar %*
 endlocal
